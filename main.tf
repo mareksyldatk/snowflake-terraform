44004dfdf5d19028b844859abbc48cdffbba6214
Marek Syldatk <marek@syldatk.com>
2026-03-02 18:43:52 +0100

    Add an example warehouse

--- main.tf
+++ main.tf
@@ -21,3 +21,15 @@ resource "snowflake_database" "tf_db" {
   name         = "TF_PLATFORM"
   is_transient = false
 }
+
+resource "snowflake_warehouse" "tf_warehouse" {
+  name                      = "TF_PLATFORM_WH"
+  warehouse_type            = "STANDARD"
+  warehouse_size            = "XSMALL"
+  max_cluster_count         = 1
+  min_cluster_count         = 1
+  auto_suspend              = 60
+  auto_resume               = true
+  enable_query_acceleration = false
+  initially_suspended       = true
+}
