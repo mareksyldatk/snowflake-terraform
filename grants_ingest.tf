2c8cf3e3e61b2bfa8ea4a7ea8746a5d9781efeb3
Marek Syldatk <marek@syldatk.com>
2026-03-03 19:59:06 +0100

    Add TF_ROLE_INGEST

---
+++ grants_ingest.tf
@@ -0,0 +1,52 @@
+resource "snowflake_grant_privileges_to_account_role" "tf_role_ingest_tf_ingest_wh_usage_operate" {
+  account_role_name = snowflake_account_role.tf_role_ingest.name
+  privileges        = ["USAGE", "OPERATE"]
+
+  on_account_object {
+    object_type = "WAREHOUSE"
+    object_name = snowflake_warehouse.tf_ingest.name
+  }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "tf_role_ingest_tf_bronze_usage" {
+  account_role_name = snowflake_account_role.tf_role_ingest.name
+  privileges        = ["USAGE"]
+
+  on_account_object {
+    object_type = "DATABASE"
+    object_name = snowflake_database.tf_bronze.name
+  }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "tf_role_ingest_tf_bronze_bronze_schema_privileges" {
+  account_role_name = snowflake_account_role.tf_role_ingest.name
+  privileges        = ["USAGE", "CREATE TABLE", "CREATE STAGE", "CREATE FILE FORMAT"]
+
+  on_schema {
+    schema_name = "${snowflake_database.tf_bronze.name}.${snowflake_schema.tf_bronze_bronze.name}"
+  }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "tf_role_ingest_tf_bronze_bronze_select_all_tables" {
+  account_role_name = snowflake_account_role.tf_role_ingest.name
+  privileges        = ["SELECT"]
+
+  on_schema_object {
+    all {
+      object_type_plural = "TABLES"
+      in_schema          = "${snowflake_database.tf_bronze.name}.${snowflake_schema.tf_bronze_bronze.name}"
+    }
+  }
+}
+
+resource "snowflake_grant_privileges_to_account_role" "tf_role_ingest_tf_bronze_bronze_select_future_tables" {
+  account_role_name = snowflake_account_role.tf_role_ingest.name
+  privileges        = ["SELECT"]
+
+  on_schema_object {
+    future {
+      object_type_plural = "TABLES"
+      in_schema          = "${snowflake_database.tf_bronze.name}.${snowflake_schema.tf_bronze_bronze.name}"
+    }
+  }
+}
